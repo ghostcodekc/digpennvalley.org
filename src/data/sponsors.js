7fc5ef7abaa79b2c661add3f7d209402628e48dc
Andrew Grube <andrewmgrube@gmail.com>
2025-07-10 21:36:47 -0500

    added sponsors

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,10 +1,10 @@
 export const SPONSORS = [
-    { id: 1, name: "Kansas City Parks & Rec", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=KC+Parks" },
-    { id: 2, name: "Better Way R+ Dog Training", logoUrl: "https://images.squarespace-cdn.com/content/v1/629c1ccbb33f845b6426bd08/941e06e5-2b88-481b-8722-eb96969a80c7/BetterWay_HI_Logo_A_2023.png?format=1500w" },
-    { id: 3, name: "Benebone", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=Benebone" },
-    { id: 4, name: "Nylabone", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=Nylabone" },
-    { id: 5, name: "RootsKC", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=RootsKC." },
-    { id: 6, name: "Union Hill Animal Hospital", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=Union+Hill+Animal+Hospital" },
-    { id: 7, name: "Brookside Barkery", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=Brookside+Barkery" },
-    { id: 8, name: "Westwood Animal Hospital", logoUrl: "https://placehold.co/200x100/e2e8f0/4a5568?text=Westwood+Animal+Hospital" }
+    { id: 1, name: "Kansas City Parks & Rec", logoUrl: "/public/images/sponsors/kc-parks-logo.png" },
+    { id: 2, name: "Better Way R+ Dog Training", logoUrl: "/public/images/sponsors/BetterWay.png" },
+    { id: 3, name: "Benebone", logoUrl: "/public/images/sponsors/benebone-logo.webp" },
+    { id: 4, name: "Nylabone", logoUrl: "/public/images/sponsors/nylabone.jpg" },
+    { id: 5, name: "RootsKC", logoUrl: "/public/images/sponsors/roots.webp" },
+    { id: 6, name: "Union Hill Animal Hospital", logoUrl: "/public/images/sponsors/uhah.gif" },
+    { id: 7, name: "Brookside Barkery", logoUrl: "/public/images/sponsors/Brookside-Barkery.webp" },
+    { id: 8, name: "Westwood Animal Hospital", logoUrl: "/public/images/sponsors/westwood.png" }
 ];
